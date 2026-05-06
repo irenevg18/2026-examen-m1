ecf3bc18f004a44344db5f8dcd8308da84708312
irenevg18 <irenevalleguillen@gmail.com>
2026-05-07 01:41:32 +0200

    first commit

---
+++ challenges.js
@@ -0,0 +1,96 @@
+/***** Ejercicio 1 **********
+ * Haz que el siguiente console.log imprima true en la consola (sin modificar el console.log)
+ */
+
+let count = 0;
+++count;
+count++;
+count + count;
+count = count + 2;
+
+const result = count === 6;
+
+console.log(result);
+
+
+/***------------------------- */
+
+/***** Ejercicio 2 **********
+ * Crea dos variables cuyo valor sea un string y concatenalos. Muestra el resultado por consola.
+ */
+
+
+//Tu código aquí.
+
+/* Ejercicio 3 ------- 
+
+Imprime un string con los nombres y los id's de los estudiantes. 
+
+*/
+
+
+const students = [
+    {id: 0, name : "Ingrid"}, 
+    {id: 1, name : "Uliana"}, 
+    {id: 2, name : "Javi"},
+    {id: 3, name : "Saba"},
+    {id: 4, name : "Helena"},
+]
+
+
+//tu código aquí
+
+
+
+/***------------------------- */
+
+/***** Ejercicio 4 **********
+ * Haz que el array people se parezca a al siguiete utilizando alguno de los siguientes métodos: (.push, .pop(), .splice(), .shift(), .unshift()) y muéstralo por consola.
+ *
+ * const people = ["Jan", "LLuna", "Mar", "Clara", "Leo"]
+ */
+
+const people = [
+  "Jan",
+  "Jordi",
+  "Lluc",
+  "LLuna",
+  "Mar",
+  "Clara",
+  "Leo",
+  "Sandra",
+];
+
+
+//Tu código aquí.
+
+
+
+/***------------------------- */
+
+
+
+/* Ejercicio 5  ------- 
+
+Escribe un programa que imprima todos los números del 1 al 100.
+
+Ahora, para todos los números que sea divisibles entre 3, imprime la palabra
+"Fizz" en vez del número.
+
+Para todos los que sean divisibles entre 5, imprime la palabra "Buzz".
+
+Y para todos aquellos números divisibles tanto entre 3 cómo entre 5,
+imprime la palabra "FizzBuzz".
+
+(pregunta típica de entrevista)
+
+*/
+
+
+
+
+
+
+//tu código aquí
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Prueba de maquetación
+
+👉 Intenta reproducir una de las siguientes opciones *(puedes hacerlo con Tailwind o CSS puro)*:
+
+- 1: ![1: navbar](./img/Captura%20de%20pantalla%202026-05-07%20a%20las%201.16.45.png)
+
+- 2: ![2: navbar 2](./img/Captura%20de%20pantalla%202026-05-07%20a%20las%201.36.37.png)
+
+- 3: ![3: cards](./img/Captura%20de%20pantalla%202026-05-07%20a%20las%201.20.24.png)
+
+- 4: ![4: cards 2](./img/Captura%20de%20pantalla%202026-05-07%20a%20las%201.32.05.png)
+
+
+*Puedes utilizar cualquier imagen y texto. No hace falta que coincidan ni la tipografía, ni el logo.*
